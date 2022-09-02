30658b754ed3ffe0609324b702bbd7816286a337
gildedhipbone <falck.jacob@gmail.com>
2022-09-02 21:15:51 +0200

    Added add-on preferences.

--- RescaleAllGrids.py
+++ RescaleAllGrids.py
@@ -1,8 +1,11 @@
+from distutils.sysconfig import customize_compiler
 import bpy
+from bpy.types import Operator, AddonPreferences
+from bpy.props import FloatProperty, StringProperty
 
 bl_info = {
-    "name": "Rescale Grids",
-    "description": "Simultaneously rescales all grids.",
+    "name": "Rescale All Grids",
+    "description": "Manually rescale all grids.", 
     "author": "Jacob Falck",
     "blender": (3, 1, 0),
     "version": (1, 0, 0),
@@ -13,19 +16,42 @@ bl_info = {
     "category": "View 3D"
 }
 
+class RescaleGridsAddonPreferences(AddonPreferences):
+    bl_idname = __name__
+
+    custom_multiplier: FloatProperty(
+        name="Grid Scale Multiplier",
+        default=1.0
+    )
+    custom_unit: StringProperty(
+        name="Unit Name",
+        default=""
+    )
+
+    def draw(self, context):
+        layout = self.layout
+        #layout.label(text="")
+        layout.prop(self, "custom_multiplier")
+        layout.prop(self, "custom_unit")
+
 class RescaleGrids(bpy.types.Operator):
     """Tooltip."""
     bl_idname = "view3d.rescale_grids"
     bl_label = "Rescale Grids Addon"
     bl_space_type = 'VIEW_3D'
     
-    grid_scalar: bpy.props.FloatProperty(default=1.0)
-    grid_scale: bpy.props.FloatProperty()
+    grid_scalar: FloatProperty(default=1.0)
+    grid_scale: FloatProperty()
     # Set to custom multiplier to whatever you want 1 Blender Unit to represent.
     # This should be exposed in the add-on settings. Also add an option to set a custom unit extension, e.g. "px".
-    custom_multiplier: bpy.props.FloatProperty(default=1.0)
 
     def execute(self, context):
+        preferences = context.preferences
+        addon_prefs = preferences.addons[__name__].preferences
+
+        custom_multiplier = addon_prefs.custom_multiplier
+        custom_name = addon_prefs.custom_unit
+
         for window in bpy.context.window_manager.windows:
             for area in window.screen.areas:
                 if not area.type == "VIEW_3D":
@@ -37,12 +63,14 @@ class RescaleGrids(bpy.types.Operator):
                         self.grid_scale = s.overlay.grid_scale
                         break
         # This should be drawn in the display port.
-        self.report({"INFO"}, f"Grid scale: {self.grid_scale * self.custom_multiplier}")
+        self.report({"INFO"}, f"Grid scale: {(self.grid_scale * custom_multiplier)} {custom_name}")
+
         return {"FINISHED"}
 
 addon_keymaps = []
 
 def register():
+    bpy.utils.register_class(RescaleGridsAddonPreferences)
     bpy.utils.register_class(RescaleGrids)
     wm = bpy.context.window_manager
     kc = wm.keyconfigs.addon
@@ -62,6 +90,7 @@ def unregister():
     addon_keymaps.clear()
 
     bpy.utils.unregister_class(RescaleGrids)
+    bpy.utils.register_class(RescaleGridsAddonPreferences)
 
 if __name__ == "__main__":
     register()
